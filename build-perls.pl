163da89f3c142d1a93824ffffd2d4b2ec3879fd4
David Golden <dagolden@cpan.org>
2014-07-02 10:52:51 -0400

    bootstrap cpanminus and avoid cpan for module installation

--- build-perls.pl
+++ build-perls.pl
@@ -19,11 +19,17 @@ my $build_dir     = Cwd::getcwd;
 my $data          = Parse::CPAN::Meta->load_file($yaml_file);
 my @common_config = split " ", $data->{common_config};
 
+# bootstrap cpanminus
+try_run( "wget", "http://xrl.us/cpanm" ) unless -f "cpanm";
+chmod 0755, "cpanm" unless -x "cpanm";
+
+# clear out environment that could confuse installation
+delete $ENV{$_} for qw/PERL5LIB PERL_MM_OPT PERL_MB_OPT/;
+
 for my $perl ( @{ $data->{perls} } ) {
-    say "Building $perl->{name}";
+    say "*** Building Perl $perl->{name} ***";
     my $prefix = File::Spec->catfile( $lang_path, $perl->{name} );
     unless ( -x File::Spec->catfile( $prefix, qw/bin perl/ ) ) {
-        chdir $build_dir;
         my @local_config = $perl->{config} ? ( split " ", $perl->{config} ) : ();
         my @config = ( @common_config, "-Dprefix=$prefix", @local_config );
         my ($tarball) = $perl->{url} =~ m{.*/(.*)};
@@ -39,19 +45,17 @@ for my $perl ( @{ $data->{perls} } ) {
         try_run( "./Configure", @config );
         try_run( "make",        "-j9" );
         try_run( "make",        "install" );
+
+        chdir $build_dir;
     }
 
     # switch to new perl for post install
-    $ENV{PATH}                      = "$prefix/bin:$ENV{PATH}";
-    $ENV{PERL_EXTUTILS_AUTOINSTALL} = "--defaultdeps";
-    delete $ENV{$_} for qw/PERL5LIB PERL_MM_OPT PERL_MB_OPT/;
-
-    try_run( 'cpan',  'App::cpanminus' );
-    try_run( 'cpanm', 'TAP::Harness::Restricted' );
+    local $ENV{PATH} = "$prefix/bin:$ENV{PATH}";
 
     # let's avoid any pod tests and prompts when we try to install stuff
-    $ENV{HARNESS_SUBCLASS} = "TAP::Harness::Restricted";
-    try_run( 'cpanm', @{ $data->{post_install} } ) if $data->{post_install};
+    try_run( './cpanm', 'TAP::Harness::Restricted' );
+    local $ENV{HARNESS_SUBCLASS} = "TAP::Harness::Restricted";
+    try_run( './cpanm', @{ $data->{post_install} } ) if $data->{post_install};
 }
 
 sub try_run {
